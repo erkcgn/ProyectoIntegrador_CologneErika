28ed546b336c839aa4f6b113be213f0b73ea7463
Erika <erikac.ec80@gmail.com>
2022-08-18 11:27:47 -0300

    cree nuevo componente edu y exp

---
+++ ProyectoIntegrador_erkcgn/src/app/componentes/educacion-yexperiencia/educacion-yexperiencia.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-educacion-yexperiencia',
+  templateUrl: './educacion-yexperiencia.component.html',
+  styleUrls: ['./educacion-yexperiencia.component.css']
+})
+export class EducacionYExperienciaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
