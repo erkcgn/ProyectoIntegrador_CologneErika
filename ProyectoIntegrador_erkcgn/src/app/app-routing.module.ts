1277d1da104ec12b30406de1095da4ac92f4ce2a
Erika <erikac.ec80@gmail.com>
2022-08-18 12:15:10 -0300

    cree routing

--- ProyectoIntegrador_erkcgn/src/app/app-routing.module.ts
+++ ProyectoIntegrador_erkcgn/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { AptitudesComponent } from './componentes/aptitudes/aptitudes.component';
+import { EducacionYExperienciaComponent } from './componentes/educacion-yexperiencia/educacion-yexperiencia.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { IniciarSesionComponent } from './componentes/iniciar-sesion/iniciar-sesion.component';
+import { LogrosComponent } from './componentes/logros/logros.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: EncabezadoComponent},
+  {path: 'iniciar-sesion', component: IniciarSesionComponent},
+  {path: 'acerca-de', component: AcercaDeComponent},
+  {path: 'educacion-yexperiencia', component: EducacionYExperienciaComponent},
+  {path: 'aptitudes', component: AptitudesComponent},
+  {path: 'aptitudes', component: AptitudesComponent},
+  {path: 'logros', component: LogrosComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
