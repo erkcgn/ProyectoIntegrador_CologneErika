747e7c141d99265292972e30221688a798ec3e24
Erika <erikac.ec80@gmail.com>
2022-08-19 10:56:28 -0300

    actualice routing con nuevos componentes

--- ProyectoIntegrador_erkcgn/src/app/app-routing.module.ts
+++ ProyectoIntegrador_erkcgn/src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
 import { AptitudesComponent } from './componentes/aptitudes/aptitudes.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
 
 import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
 import { IniciarSesionComponent } from './componentes/iniciar-sesion/iniciar-sesion.component';
 import { LogrosComponent } from './componentes/logros/logros.component';
 
@@ -11,7 +13,8 @@ const routes: Routes = [
   {path: '', component: EncabezadoComponent},
   {path: 'iniciar-sesion', component: IniciarSesionComponent},
   {path: 'acerca-de', component: AcercaDeComponent},
-  
+  {path: 'experiencia', component: ExperienciaComponent},
+  {path: 'educacion', component: EducacionComponent},  
   {path: 'aptitudes', component: AptitudesComponent},
   {path: 'aptitudes', component: AptitudesComponent},
   {path: 'logros', component: LogrosComponent},
