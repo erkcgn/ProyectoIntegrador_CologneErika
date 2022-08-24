1f107017490f41d26dfc2bc2a10a4b010d4693b7
Erika <erikac.ec80@gmail.com>
2022-08-24 16:59:55 -0300

    genere spring boot

---
+++ ProyectoIntegrador_erkcgn/BackEnd/src/main/java/com/portfolio/erkcgn/ErkcgnApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.erkcgn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErkcgnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErkcgnApplication.class, args);
+	}
+
+}
---
+++ ProyectoIntegrador_erkcgn/BackEnd/src/test/java/com/portfolio/erkcgn/ErkcgnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.erkcgn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErkcgnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
