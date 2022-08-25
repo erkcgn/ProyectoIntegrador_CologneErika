e1d13fc2b551d0c75d89823ba7b16f118fa8d155
Erika <erikac.ec80@gmail.com>
2022-08-25 17:09:20 -0300

    creando login

--- ProyectoIntegrador_erkcgn/FrontEnd Angular/src/app/componentes/encabezado/encabezado.component.ts
+++ ProyectoIntegrador_erkcgn/FrontEnd Angular/src/app/componentes/encabezado/encabezado.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-encabezado',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EncabezadoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
-
+  
+  login(){
+    this.router.navigate(['/iniciar-sesion'])
+  }
 }
