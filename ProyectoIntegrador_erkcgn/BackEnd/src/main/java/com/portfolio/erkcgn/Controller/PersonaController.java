626b573a0f3a0e28c86ebb4b447c6c7237bedeca
Erika <erikac.ec80@gmail.com>
2022-08-25 16:18:15 -0300

    import crossorigin error CORS

--- ProyectoIntegrador_erkcgn/BackEnd/src/main/java/com/portfolio/erkcgn/Controller/PersonaController.java
+++ ProyectoIntegrador_erkcgn/BackEnd/src/main/java/com/portfolio/erkcgn/Controller/PersonaController.java
@@ -5,6 +5,7 @@ import com.portfolio.erkcgn.Entity.Persona;
 import com.portfolio.erkcgn.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
