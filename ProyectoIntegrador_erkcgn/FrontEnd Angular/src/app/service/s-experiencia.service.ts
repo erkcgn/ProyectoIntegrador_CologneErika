bdfa9215af560f662c1128868e4fe968399785fb
Erika <erikac.ec80@gmail.com>
2022-08-29 20:21:02 -0300

    corregi metodo delete

--- ProyectoIntegrador_erkcgn/FrontEnd Angular/src/app/service/s-experiencia.service.ts
+++ ProyectoIntegrador_erkcgn/FrontEnd Angular/src/app/service/s-experiencia.service.ts
@@ -8,7 +8,7 @@ import { Experiencia } from '../model/experiencia';
 })
 export class SExperienciaService {
   
-  expURL = 'http://localhost:8080/explab';
+  expURL = 'http://localhost:8080/explab/';
 
   constructor(private httpClient: HttpClient) { }
 
@@ -29,6 +29,6 @@ export class SExperienciaService {
   }
 
   public delete(id: number): Observable<any>{
-    return this.httpClient.delete<any>(this.expURL + `detele/${id}`);
+    return this.httpClient.delete<any>(this.expURL + `delete/${id}`);
   }
 }
