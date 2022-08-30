2bd1635855d60cdef3c636bdadd6c0090b617ddb
Erika <erikac.ec80@gmail.com>
2022-08-30 20:07:00 -0300

    actualice links de acuerdo a deploy back  front firebase

--- ProyectoIntegrador_erkcgn/BackEnd/src/main/java/com/portfolio/erkcgn/Controller/PersonaController.java
+++ ProyectoIntegrador_erkcgn/BackEnd/src/main/java/com/portfolio/erkcgn/Controller/PersonaController.java
@@ -17,11 +17,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portfolioerkcgn.web.app")
+
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
-    @GetMapping("personas/mostrar")
+    @GetMapping("/personas/mostrar")
     public List<Persona> getPersona(){
         return ipersonaService.getPersona();
     }
