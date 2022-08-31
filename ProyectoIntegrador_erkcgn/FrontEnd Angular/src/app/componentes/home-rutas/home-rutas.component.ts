efcfc236bb005269431e8658e2efd695a47e3f9e
Erika <erikac.ec80@gmail.com>
2022-08-31 19:49:42 -0300

    nuevo componente para ruteo

---
+++ ProyectoIntegrador_erkcgn/FrontEnd Angular/src/app/componentes/home-rutas/home-rutas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-rutas',
+  templateUrl: './home-rutas.component.html',
+  styleUrls: ['./home-rutas.component.css']
+})
+export class HomeRutasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
