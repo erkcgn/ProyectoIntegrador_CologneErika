da7222aea6a3e854881db21b91fb2e5361818d56
Erika <erikac.ec80@gmail.com>
2022-08-31 13:46:04 -0300

    modifique URL para pruebas locales

--- ProyectoIntegrador_erkcgn/FrontEnd Angular/src/app/service/persona.service.ts
+++ ProyectoIntegrador_erkcgn/FrontEnd Angular/src/app/service/persona.service.ts
@@ -8,7 +8,7 @@ import { persona } from '../model/persona.model';
 })
 export class PersonaService {
   
-  URL = 'https://bckportf.herokuapp.com/personas/';
+  URL = 'http://localhost:8080/personas/';
 
   constructor(private http: HttpClient) { }
 
--- ProyectoIntegrador_erkcgn/FrontEnd Angular/src/app/service/s-experiencia.service.ts
+++ ProyectoIntegrador_erkcgn/FrontEnd Angular/src/app/service/s-experiencia.service.ts
@@ -8,7 +8,7 @@ import { Experiencia } from '../model/experiencia';
 })
 export class SExperienciaService {
   
-  expURL = 'https://bckportf.herokuapp.com/explab/';
+  expURL = 'http://localhost:8080/explab/';
 
   constructor(private httpClient: HttpClient) { }
 
