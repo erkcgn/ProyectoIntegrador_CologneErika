87a7327d6a2d3a324a779f9c4ff037fa9d179caf
Erika <erikac.ec80@gmail.com>
2022-09-12 11:12:59 -0300

    trabajando sobre funcionalidad

--- ProyectoIntegrador_erkcgn/BackEnd/src/main/java/com/portfolio/erkcgn/Controller/PersonaController.java
+++ ProyectoIntegrador_erkcgn/BackEnd/src/main/java/com/portfolio/erkcgn/Controller/PersonaController.java
@@ -11,7 +11,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
