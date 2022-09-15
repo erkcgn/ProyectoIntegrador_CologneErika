3f771991fcd3193c732622aaa73895f9d7d2ae4e
Erika <erikac.ec80@gmail.com>
2022-09-15 17:04:00 -0300

    cree front hard y soft skills para que conecte con back

--- ProyectoIntegrador_erkcgn/FrontEnd Angular/src/app/componentes/aptitudes/aptitudes.component.ts
+++ ProyectoIntegrador_erkcgn/FrontEnd Angular/src/app/componentes/aptitudes/aptitudes.component.ts
@@ -1,17 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { NgCircleProgressModule } from 'ng-circle-progress';
+import { Skill } from 'src/app/model/skill';
+import { SkillService } from 'src/app/service/skill.service';
+import { TokenService } from 'src/app/service/token.service';
 @Component({
   selector: 'app-aptitudes',
   templateUrl: './aptitudes.component.html',
   styleUrls: ['./aptitudes.component.css']
 })
 export class AptitudesComponent implements OnInit {
+  skill: Skill[] = [];
+  
 
-  constructor(
-    
-  ) { }
+  constructor(private skillS: SkillService,
+              private tokenService: TokenService) { }
+
+  isLogged = false;
 
   ngOnInit(): void {
+    this.cargarSkill();
+    if(this.tokenService.getToken()){
+      this.isLogged = true;
+    }else {
+      this.isLogged = false;
+    }
+  }
+
+  cargarSkill(): void {
+    this.skillS.lista().subscribe(skills => {
+      this.skill = skills;
+    })
+  }
+
+  eliminar(id: number){
+    if(id != undefined){
+      this.skillS.delete(id).subscribe(data=>{
+        this.cargarSkill();
+      },err => {
+        alert("No se pudo eliminar");
+      });
+    }
   }
 
 }
